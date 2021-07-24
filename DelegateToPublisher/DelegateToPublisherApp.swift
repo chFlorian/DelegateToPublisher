f588065ce50dda9768848bb970ff026c538acedd
florian schweizer <florian.schweizer@tum.de>
2021-07-24 14:30:44 +0200

    Initial Commit

---
+++ DelegateToPublisher/DelegateToPublisherApp.swift
@@ -0,0 +1,17 @@
+//
+//  DelegateToPublisherApp.swift
+//  DelegateToPublisher
+//
+//  Created by florian schweizer on 24.07.21.
+//
+
+import SwiftUI
+
+@main
+struct DelegateToPublisherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
